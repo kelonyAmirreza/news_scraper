052564301ffed3fff18b5a3f879c555e9b076fac
Amirreza <ghodrati.amirreza@gmail.com>
2023-04-18 22:10:44 +0200

    gpt added

--- main.py
+++ main.py
@@ -5,23 +5,33 @@ from typing import List, Dict
 from datetime import date, timedelta
 
 from newsapi import NewsApiClient
+import openai
 
 
 def main():
-    init()
+    api_client_chosen = init()
 
-    print(news_api_get_articles(["mode", "fashion"]))
-    # for article in all_articles["articles"]:
-    #     print(article, end="\n"*3)
+    if api_client_chosen == "newsapi":
+
+        articles = news_api_get_articles(["mode", "fashion"])
+        for article in articles["articles"]:
+            print(article, end="\n"*3)
+
+    else:
+        response = openao_get_articles(["mode", "fashion"], "trend")
+
+        for result in response.choices[0].text.split("\n"):
+            print(result)
 
 
-def init():
+def init() -> str:
+    """
+        Importing the newsapi API key from the .env file and create the api client
+    """
+
     load_dotenv()
 
     if len(argv) == 2 and argv[1] == "n":
-        """
-        Importing the newsapi API key from the .env file and create the api client
-        """
 
         global NEWSAPI_API_CLIENT
         NEWSAPI_API_KEY = os.getenv("NEWSAPI_API_KEY")
@@ -29,11 +39,19 @@ def init():
             raise EnvironmentError("NEWSAPI_API_KEY not set correctly")
         NEWSAPI_API_CLIENT = NewsApiClient(api_key=NEWSAPI_API_KEY)
 
+        return "newsapi"
+
     else:
-        """
-        Importing the newsapi API key from the .env file and create the api client
-        """
-        print(2)
+        OPENAI_ORGANIZATION = os.getenv("OPENAI_ORGANIZATION")
+        OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+        if not OPENAI_ORGANIZATION or not OPENAI_API_KEY:
+            raise EnvironmentError("OPEN AI API_KEYS not set correctly")
+
+        openai.organization = OPENAI_ORGANIZATION
+        openai.api_key = OPENAI_API_KEY
+
+        return "chatgpt"
 
 
 def news_api_get_articles(keywords: List[str], sources: List[str] = None, domains: List[str] = None,
@@ -65,6 +83,50 @@ def news_api_get_articles(keywords: List[str], sources: List[str] = None, domain
                                                  language=lang,
                                                  sort_by=sort_by)
     return articles
+
+
+def openao_get_articles(keywords: List[str], category: str = "people",
+                        model_engine: str = "text-davinci-003", max_tokens: int = 4000, max_results: int = 5):
+    """
+    Uses OpenAI's GPT-3 API to generate articles related to the given keywords.
+
+    Args:
+    keywords: A list of strings representing the keywords related to the article you want to generate.
+    category:  A string representing the type of article you want to generate. (optional, Defaults to "people").
+                     Only accepts values of "people", "trend", or "history".
+    model_engine: A string representing the OpenAI engine used to generate the article. (optional, Defaults to "text-davinci-003").
+    max_tokens:  An integer representing the maximum number of tokens used in the generated article. (optional, Defaults to 1024).
+    max_results: An integer representing the maximum number of results to return. (optional, Defaults to 5).
+
+    Errors:
+    raises ValueError: If an invalid category parameter is provided.
+
+    Returns:
+    A dictionary containing the generated article and any other relevant information returned by the OpenAI API.
+    """
+
+    if category != "people" and category != "trend" and category != "history":
+        raise ValueError(
+            "Wrong category selecte. Only (people, trend and history supported)")
+
+    # prompt_people = f"List the what most famous celebrities or influencers related to {keywords} did or wear in the last few days and news around them."
+    prompt_people = f"Can you retrieve the latest news and updates related to {keywords} for top celebrities and influencers? Please include any recent fashion choices or noteworthy actions taken by them in the past few days."
+
+    prompt_trend = f"Find the latest trends on {keywords} and see what the trends are saying. and say it in informative tone"
+
+    history_prompt = f"GPT, please find the most significant historical events associated with {keywords} for the leading companies in the sector."
+
+    if category == "people":
+        prompt = prompt_people
+    elif category == "trend":
+        prompt = prompt_trend
+    else:
+        prompt = history_prompt
+
+    response = openai.Completion.create(engine=model_engine, prompt=prompt, max_tokens=max_tokens,
+                                        n=1, stop=None, temperature=0.5, frequency_penalty=0, presence_penalty=0)
+
+    return response
 
 
 main()
